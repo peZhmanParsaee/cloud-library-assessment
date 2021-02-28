850e5be150746c4643c715343483bb06bea43384
pezhmanparsaee <p.parsaee@gmail.com>
2021-02-28 16:58:04 +0330

    init

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
