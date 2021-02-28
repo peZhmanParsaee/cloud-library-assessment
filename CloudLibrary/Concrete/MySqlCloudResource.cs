53cc441ff89777216262a5a59c801826b13ad3a1
pezhmanparsaee <p.parsaee@gmail.com>
2021-02-28 17:02:01 +0330

    implement inherited method & props in MySqlCloudResource class

--- CloudLibrary/Concrete/MySqlCloudResource.cs
+++ CloudLibrary/Concrete/MySqlCloudResource.cs
@@ -4,5 +4,32 @@ using CloudLibrary.Helpers;
 namespace CloudLibrary.Concrete {
   public class MySqlCloudResource : AbstractDatabaseServer
   {
+    #region Props (4)
+
+    public override string FolderName { get { return "DatabaseServer"; } }
+    public override string JsonFileName { get { return "MySql"; } }
+    public override string Host { get; set; }
+    public override string DatabaseName { get; set; }
+    public override string UserName { get; set; }
+    public override string Password { get; set; }
+
+
+    #endregion
+
+    #region Methods (1)
+
+    public override string ToJson()
+    {
+      var jsonData = new {
+        Host,
+        DatabaseName,
+        UserName,
+        Password
+      };
+
+      return JsonHelper.SerializeResouceContents(jsonData);
+    }
+
+    #endregion    
   }
 }
