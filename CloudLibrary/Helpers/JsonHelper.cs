8e4329fe231c792cc8f1ba23b02b6ffbffafe544
pezhmanparsaee <p.parsaee@gmail.com>
2021-02-28 17:03:44 +0330

    add SerializeResouceContents() static method in JsonHelper class

--- CloudLibrary/Helpers/JsonHelper.cs
+++ CloudLibrary/Helpers/JsonHelper.cs
@@ -2,5 +2,13 @@ using System.Text.Json;
 
 namespace CloudLibrary.Helpers {
   public static class JsonHelper {
+    public static string SerializeResouceContents(object data) {
+      var options = new JsonSerializerOptions() {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true
+      };
+      var jsonString = JsonSerializer.Serialize(data, options);
+      return jsonString;
+    }
   }
 }
